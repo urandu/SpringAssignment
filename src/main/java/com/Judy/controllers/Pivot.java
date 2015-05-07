fe1a45790604f101cf886082299259b350c651e6
urandu <bildadnamawa@gmail.com>
2015-05-07 03:11:08 +0300

    added all java files

--- src/main/java/com/Judy/controllers/Pivot.java
+++ src/main/java/com/Judy/controllers/Pivot.java
@@ -1,6 +1,8 @@
 package com.Judy.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Pivot {
     @RequestMapping("/")
-    public String welcome()
+    public String welcome(Model model)
     {
         return "patients";
     }
